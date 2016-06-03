2f8713c510086059bbf6d5b3ec749f08f16e2873
Jordi Prats <jprats@systemadmin.es>
2016-06-03 18:50:45 +0200

    :D

--- manifests/master/postgres.pp
+++ manifests/master/postgres.pp
@@ -2,7 +2,7 @@ class spacewalk::master::postgres() inherits spacewalk {
 
 #  spacewalk-setup-postgresql.noarch : Tools to setup embedded PostgreSQL database for Spacewalk
 
-# [root@spacewalk ~]# spacewalk-setup-postgresql --db spacedb --user spaceuser --password spacepassword
+# [root@spacewalk ~]# spacewalk-setup-postgresql create --db spacedb --user spaceuser --password spacepassword
 
 
 }

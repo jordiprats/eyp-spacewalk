05c9bcb1f95b85c681a594d84bbd19a28ab7073f
Jordi Prats <jprats@systemadmin.es>
2016-07-06 13:47:43 +0200

    bugfix is_docker_container

--- manifests/service.pp
+++ manifests/service.pp
@@ -7,9 +7,10 @@ class spacewalk::service inherits spacewalk {
 
   validate_re($spacewalk::service_ensure, [ '^running$', '^stopped$' ], "Not a valid daemon status: ${spacewalk::service_ensure}")
 
-  if(getvar('::eyp_docker_iscontainer')==undef or
-      getvar('::eyp_docker_iscontainer')==false or
-      getvar('::eyp_docker_iscontainer') =~ /false/ or
+  $is_docker_container_var=getvar('::eyp_docker_iscontainer')
+  $is_docker_container=str2bool($is_docker_container_var)
+
+  if( $is_docker_container==false or
       $spacewalk::manage_docker_service)
   {
     if($spacewalk::manage_service)
